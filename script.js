c8c86d95bcecd36d69a2ba5cba48e8e515c2f8f0
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-25 08:06:32 -0300

    atualizando o valor total da despesa

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const category = document.getElementById("category");
 
 const expenseList = document.querySelector("ul");
 const expensesQuantity = document.querySelector("aside header p span");
+const expensesTotal = document.querySelector("aside header h2");
 amount.oninput = () => {
   let value =amount.value = amount.value.replace(/[^0-9]/g, "");
   value = Number(value) / 100
@@ -67,8 +68,18 @@ function expenseAdd(newExpense) {
 function updateTotals() {
   try{
     const items = expenseList.children
-    console.log(items)
     expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+    let total = 0
+    for (let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount")
+      let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")  
+      value = parseFloat(value)
+      if(isNaN(value)){
+        return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+      }
+      total += Number(value)
+    }
+    expensesTotal.textContent = total
   } catch (error) {
     console.log(error)
     alert("Não foi possível atualizar os totais.")
