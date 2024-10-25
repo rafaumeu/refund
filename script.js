0fcdd2a3aa286db1b7a80e331ac1e391213abdf8
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-25 06:51:50 -0300

    criando a lista e adicionando ícone conforme categoria selecionada

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
+
+const expenseList = document.querySelector("ul");
 amount.oninput = () => {
   let value =amount.value = amount.value.replace(/[^0-9]/g, "");
   value = Number(value) / 100
@@ -31,7 +33,14 @@ form.onsubmit = (event) => {
 
 function expenseAdd(newExpense) {
   try {
-    
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `./img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+    expenseItem.append(expenseIcon)
+    expenseList.append(expenseItem)
+
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.")
     console.log(error)
