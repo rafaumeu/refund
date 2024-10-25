17f09df94e449706ac66691cb7fcfc70d56878ba
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-25 07:35:28 -0300

    Adicionado o valor da despesa

--- script.js
+++ script.js
@@ -44,9 +44,14 @@ function expenseAdd(newExpense) {
     expenseName.textContent = newExpense.expense;
     const expenseCategory = document.createElement("span");
     expenseCategory.textContent = newExpense.category_name;
-
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`;
+   
     expenseInfo.append(expenseName, expenseCategory);
-    expenseItem.append(expenseIcon, expenseInfo);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
     expenseList.append(expenseItem)
 
   } catch (error) {
